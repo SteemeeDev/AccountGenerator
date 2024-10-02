02c27a79835f7468fd156d8d8104aa920aedbef4
JJoe <48448023+JJoeDev@users.noreply.github.com>
2024-10-02 15:04:12 +0200

    working :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
+#include "../vendor/imgui.h"
+#include "../vendor/backends/imgui_impl_glfw.h"
+#include "../vendor/backends/imgui_impl_opengl3.h"
+
+#include <GLFW/glfw3.h>
+
 int main(){
-    std::clog << "Hello, World!\n";
-    std::cerr << "Hello, World!\n";
-    std::cout << "Hello, World!\n";
+    if(!glfwInit()){
+        std::cerr << "GLFW INIT ERROR\n";
+        return -1;
+    }
+
+    const char* glsl_version = "#version 330";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Hello, OpenGL", nullptr, nullptr);
+    if(!window){
+        std::cerr << "GLFW WINDOW CREATION ERROR\n";
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+    }
+
     return 0;
 }

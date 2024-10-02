ba4433d820fef17d16dc2742d983ce3d51a0c79f
JJoe <48448023+JJoeDev@users.noreply.github.com>
2024-10-02 15:33:07 +0200

    structure

---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jhknu on 02/10/2024.
+//
+
+#include "Application.h"
---
+++ src/Application.h
@@ -0,0 +1,16 @@
+//
+// Created by jhknu on 02/10/2024.
+//
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+
+class Application {
+
+};
+
+
+
+#endif //APPLICATION_H
